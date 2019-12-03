947afc21f96717ca24db038e875490da44a88a83
Peter van Dijk <peter.van.dijk@bynder.com>
2019-12-03 22:28:46 +0100

    solved day3 puzzle1, but very in-efficient code! 60sec..

--- src/3/index.ts
+++ src/3/index.ts
@@ -1,5 +1,5 @@
-// import wirePaths from "./input";
-import wirePaths from "./testInput1";
+import wirePaths from "./input";
+// import wirePaths from "./testInput3";
 
 enum Direction {
   LEFT = "LEFT",
@@ -65,26 +65,92 @@ const makeMove = (
   }
 };
 
+const inBetweenSteps = (
+  pos1: GridPosition,
+  pos2: GridPosition
+): GridPosition[] => {
+  if (pos1.x === pos2.x) {
+    const nrOfSteps = pos2.y - pos1.y;
+    const step = nrOfSteps < 0 ? -1 : 1;
+    const steps: GridPosition[] = [];
+
+    for (let i = 1; i <= Math.abs(nrOfSteps); i++) {
+      steps.push({ x: pos2.x, y: pos1.y + step * i });
+    }
+
+    return steps;
+  } else if (pos1.y === pos2.y) {
+    const nrOfSteps = pos2.x - pos1.x;
+    const step = nrOfSteps < 0 ? -1 : 1;
+    const steps: GridPosition[] = [];
+
+    for (let i = 1; i <= Math.abs(nrOfSteps); i++) {
+      steps.push({ x: pos1.x + step * i, y: pos2.y });
+    }
+    return steps;
+  } else {
+    throw new Error();
+  }
+};
+
 const wire1Positions: GridPosition[] = [{ x: 0, y: 0 }];
 const wire2Positions: GridPosition[] = [{ x: 0, y: 0 }];
 
-wirePaths.wire1.forEach((instr: string) => {
+wirePaths.wire1.map(instr => {
+  const previousPosition = wire1Positions[wire1Positions.length - 1];
+
   const positionAfterMove = makeMove(
     processInstruction(instr),
-    wire1Positions[wire1Positions.length - 1]
+    previousPosition
   );
-  wire1Positions.push(positionAfterMove);
+
+  const newSteps = inBetweenSteps(previousPosition, positionAfterMove);
+  newSteps.forEach(step => {
+    wire1Positions.push(step);
+  });
+  // wire1Positions.push(positionAfterMove);
 });
 
-wirePaths.wire2.forEach((instr: string) => {
+wirePaths.wire2.map(instr => {
+  const previousPosition = wire2Positions[wire2Positions.length - 1];
   const positionAfterMove = makeMove(
     processInstruction(instr),
-    wire2Positions[wire2Positions.length - 1]
+    previousPosition
   );
-  wire2Positions.push(positionAfterMove);
-  if (wire1Positions.includes(positionAfterMove)) {
-    console.log("intersection!", positionAfterMove);
+
+  const newSteps = inBetweenSteps(previousPosition, positionAfterMove);
+  newSteps.forEach(step => {
+    wire2Positions.push(step);
+  });
+  // wire2Positions.push(positionAfterMove);
+});
+
+interface Intersection extends GridPosition {
+  dist: number;
+}
+const intersections: Intersection[] = [];
+
+wire2Positions.forEach(pos => {
+  const intersec = wire1Positions.find(
+    w1pos => w1pos.x === pos.x && w1pos.y === pos.y
+  );
+  if (intersec && intersec.x !== 0 && intersec.y !== 0) {
+    intersections.push({
+      ...intersec,
+      dist: Math.abs(intersec.x) + Math.abs(intersec.y)
+    });
   }
 });
 
-console.log({ wire1Positions, wire2Positions });
+const closestIntersection = intersections.reduce((acc, val) => {
+  if (val.dist < acc.dist) {
+    return val;
+  }
+  return acc;
+});
+
+// const manhattanDistOrigin = closestIntersection.x + closestIntersection.y;
+
+console.log({ intersections });
+console.log({ closestIntersection });
+// console.log({ manhattanDistOrigin });
