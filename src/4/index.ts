8221291b27e21dd556c73e71ca55ac8865255e0a
Peter van Dijk <peter.van.dijk@bynder.com>
2019-12-04 07:43:37 +0100

    setup day4

---
+++ src/4/index.ts
@@ -0,0 +1,12 @@
+const input = "273025-767253";
+const rangeLowest = 273025;
+const rangeHighest = 767253;
+const strDigits = rangeLowest.toString().split("");
+const digits = strDigits.map(Number);
+
+console.log({ digits });
+
+let count = 0;
+let pass = rangeLowest;
+
+while (true) {}
